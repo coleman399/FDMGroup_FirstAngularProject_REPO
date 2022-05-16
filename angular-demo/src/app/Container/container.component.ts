dd97b1146ad7333383f0c4ccda90e127c130417f
dillon.coleman <coleman399@gmail.com>
2022-05-16 15:24:45 -0700

    init commit

---
+++ angular-demo/src/app/Container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'container',
+  templateUrl: './Container.component.html',
+  styleUrls: ['./Container.component.css']
+})
+export class ContainerComponent {
+
+}
