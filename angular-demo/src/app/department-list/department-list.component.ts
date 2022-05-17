12279980a059d946e851319ecc5c5bfc7ac7e4d0
dillon.coleman <coleman399@gmail.com>
2022-05-17 08:37:58 -0700

    added images and mouseover events

--- angular-demo/src/app/department-list/department-list.component.ts
+++ angular-demo/src/app/department-list/department-list.component.ts
@@ -3,18 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-department-list',
   template: `
-    <p>
-      department-list works!
-    </p>
+    <div>
+      <br>
+      <img [src]="fdmLogoSrc" style="max-width: 100px; max-height: 100px;" (mouseover)="changeImage()" (mouseleave)="resetImage()">
+    </div>
   `,
   styles: [
   ]
 })
 export class DepartmentListComponent implements OnInit {
 
-  constructor() { }
+  fdmLogoSrc: string = '../assets/images/Fdm-logo-white.jpg';
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  resetImage(): void {
+    this.fdmLogoSrc = '../assets/images/Fdm-logo-white.jpg';
+  }
+
+  changeImage(): void {
+    this.fdmLogoSrc = '../assets/images/Fdm-logo-black.png';
   }
 
 }
--- angular-demo/src/app/employee-list/employee-list.component.ts
+++ angular-demo/src/app/employee-list/employee-list.component.ts
@@ -3,15 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-list',
   template: `
-    <p>
-      employee-list works!
-    </p>
+    <div>
+      <br>
+      {{name}}
+    </div>
   `,
   styles: [
   ]
 })
 export class EmployeeListComponent implements OnInit {
 
+  name: string = "Dillon Coleman"
   constructor() { }
 
   ngOnInit(): void {
