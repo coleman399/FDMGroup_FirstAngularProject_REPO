e22a2d29ffae338236006a965291c201fa31035b
dillon.coleman <coleman399@gmail.com>
2022-05-18 09:13:58 -0700

    added service

--- angular-demo/src/app/components/contacts/contacts.component.ts
+++ angular-demo/src/app/components/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from 'src/app/interfaces/contact';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contacts',
@@ -12,22 +13,8 @@ export class ContactsComponent implements OnInit {
   selectedContact!: Contact;
   favoriteContacts: Contact[] = [];
 
-  constructor() {
-    this.contacts = [
-
-      { contactId: 1, firstName: 'Andre', middleName: 'M', lastName: 'Kolber', email: 'andre.kolber@email.com', phoneNumber: '+1 (111) 123 - 1234' },
-
-      { contactId: 2, firstName: 'Dillon', middleName: 'N/A', lastName: 'Coleman', email: 'dillon.coleman@email.com', phoneNumber: '+1 (111) 423 - 1294' },
-
-      { contactId: 3, firstName: 'Jeff', middleName: 'N/A', lastName: 'Thomassen', email: 'jeff.thomassen@email.com', phoneNumber: '+1 (355) 776 - 2475' },
-
-      { contactId: 4, firstName: 'Liam', middleName: 'N/A', lastName: 'Harrington', email: 'liam.harrington@email.com', phoneNumber: '+1 (111) 423 - 1294' },
-
-      { contactId: 5, firstName: 'Patrick', middleName: 'N/A', lastName: 'Hedquist', email: 'patrick.hedquist@email.com', phoneNumber: '+1 (111) 423 - 1294' },
-
-      { contactId: 6, firstName: 'Linette', middleName: 'N', lastName: 'Mora', email: 'linette.mora@email.com', phoneNumber: '+1 (111) 423 - 1294' }
-
-    ]
+  constructor(private contactService: ContactService) {
+    this.contacts = contactService.getContacts();
   }
 
   ngOnInit(): void {
--- angular-demo/src/app/services/contact.service.ts
+++ angular-demo/src/app/services/contact.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Contact } from '../interfaces/contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  contacts!: Contact[];
+
+  constructor() {
+
+    this.contacts = [
+
+      { contactId: 1, firstName: 'Andre', middleName: 'M', lastName: 'Kolber', email: 'andre.kolber@email.com', phoneNumber: '+1 (111) 123 - 1234' },
+
+      { contactId: 2, firstName: 'Dillon', middleName: 'N/A', lastName: 'Coleman', email: 'dillon.coleman@email.com', phoneNumber: '+1 (111) 423 - 1294' },
+
+      { contactId: 3, firstName: 'Jeff', middleName: 'N/A', lastName: 'Thomassen', email: 'jeff.thomassen@email.com', phoneNumber: '+1 (355) 776 - 2475' },
+
+      { contactId: 4, firstName: 'Liam', middleName: 'N/A', lastName: 'Harrington', email: 'liam.harrington@email.com', phoneNumber: '+1 (111) 423 - 1294' },
+
+      { contactId: 5, firstName: 'Patrick', middleName: 'N/A', lastName: 'Hedquist', email: 'patrick.hedquist@email.com', phoneNumber: '+1 (111) 423 - 1294' },
+
+      { contactId: 6, firstName: 'Linette', middleName: 'N', lastName: 'Mora', email: 'linette.mora@email.com', phoneNumber: '+1 (111) 423 - 1294' }
+
+    ]
+  }
+
+  getContacts(): Contact[] {
+    return this.contacts;
+  }
+
+  getContact(id: number) {
+    let foundContact = this.contacts.find((contact) => { contact.contactId = id})
+    return foundContact;
+  }
 }
