34a54592d4c7b5c033f2d2a02d304ec2bbabf0e9
dillon.coleman <coleman399@gmail.com>
2022-05-18 07:28:30 -0700

    added contacts

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    routingComponents
+    routingComponents,
+    ContactsComponent,
+    ContactDetailsComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ angular-demo/src/app/interfaces/contact.ts
@@ -0,0 +1,8 @@
+export interface Contact {
+  contactId: number;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
